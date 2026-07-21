4b3fa18c70fa86be5d108d7495b428cfd6c63dec
shane haddock <haddocs@tcd.ie>
2026-07-21 22:02:29 +0900

    basic show view

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Recipe.create!(
+  title: "Donuts",
+  description: "Delicious doughy treats",
+  instructions: "Mix dough and stuff and mix dough and stuff and mix dough and stuff and mix dough and stuff and mix dough and stuff and mix dough and stuff and mix dough and stuff and mix dough and stuff and ",
+  difficulty: 3,
+  rating: 3,
+  servings: 3,
+  calories: 3000,
+  prep_time: 20,
+  cook_time: 20,
+  user_id: 1
+)
