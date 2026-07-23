09fde79a2ff6984826a62865405dc4ba0289add9
shane haddock <haddocs@tcd.ie>
2026-07-23 20:58:16 +0900

    added bootstrap, simple image api for each recipe show, fallback image and finished first pass of show page. Consider markdown/formatting for recipe.instructions since its just a wall of text now

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,6 +5,9 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
+    @banner = Rails.cache.fetch("recipe_banner_#{@recipe.id}", expires_in: 1.hour) do
+      RecipeApiService.fetch_recipe_banner(@recipe)
+    end
   end
 
   def new
