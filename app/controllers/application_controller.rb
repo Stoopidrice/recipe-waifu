9d8dccd5c8119a0096f7aba404aa158c6c5800cb
Jared Legault <aniwhistler@gmail.com>
2026-07-24 21:00:47 +0900

    Updated user strong params.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[dislikes preferences])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[dislikes preferences])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[dislikes preferences username])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[dislikes preferences username])
   end
 end
