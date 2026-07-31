d807ec0fd1b15593cba534c77630750db64a8bb7
Samantha Fernandes <samsam.sammy.f@gmail.com>
2026-07-31 20:48:19 +0900

    background now scrolls. Also prettied up the home page

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,5 +1,5 @@
 require "ruby_llm/schema"
 RubyLLM.configure do |config|
-  config.openai_api_key = ENV["GITHUB_TOKEN"]
+  config.openai_api_key = ENV["OPENROUTER_API_KEY"]
   config.openai_api_base = "https://models.inference.ai.azure.com"
 end
